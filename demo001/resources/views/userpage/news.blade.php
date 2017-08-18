c108fbd9cc13a4a3e9e02f8f1d54cadf501201e9
MaoChenda <mao.chenda60@gmail.com>
2017-08-18 09:28:16 +0700

    add pictures and some information to News page

--- demo001/resources/views/userpage/news.blade.php
+++ demo001/resources/views/userpage/news.blade.php
@@ -17,12 +17,55 @@
         <div class="headline">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-2 col-xs-2">
 
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="headlineImg">
+                            <img src="{{asset('web-comp/frieght_forward.jpg')}}" alt="frieght_forward">
+                            <p>Docker Basic - With AWS EC2 Container </p>
+                            <span class="glyphicon glyphicon-time"> 2 months ago</span>
+                        </div>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-10 col-xs-10">
-                        <h3>Headlines</h3>
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="headlineImg">
+                            <img src="{{asset('web-comp/air-logistic.jpg')}}" alt="air-logistic">
+                            <p>Introducing The All New Trello Business</p>
+                            <span class="glyphicon glyphicon-time"> 1 month ago</span>
+                        </div>
                     </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="headlineImg">
+                            <img src="{{asset('web-comp/sea-port.jpg')}}" alt="sea-port">
+                            <p>Basic Docker Setup with Openshift</p>
+                            <span class="glyphicon glyphicon-time"> 4 months ago</span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="headlineImg">
+                            <img src="{{asset('web-comp/export_brokerage.jpg')}}" alt="export_brokerage">
+                            <p>Getting prepared for learning Angular 2</p>
+                            <span class="glyphicon glyphicon-time"> 2 months ago</span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="headlineImg">
+                            <img src="{{asset('web-comp/intermodal_main.jpg')}}" alt="intermodal_main">
+                            <p>PHP 7 is coming - It will gonna fly high</p>
+                            <span class="glyphicon glyphicon-time"> 1 years ago</span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="headlineImg">
+                            <img src="{{asset('web-comp/port-logistic.jpg')}}" alt="port-logistic">
+                            <p>Introduction to MEAN Stack</p>
+                            <span class="glyphicon glyphicon-time"> 2 years ago</span>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
